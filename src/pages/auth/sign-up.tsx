67540159e8b2c35f5cd29af4aeec2d65dbb23d54
Vitor Andes <vitor.andes.santos04@gmail.com>
2024-05-23 17:40:30 -0300

    ''

--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -28,6 +28,7 @@ export function SignUp() {
 
   async function handleSignUp(data: SignUpForm) {
     try {
+      console.log(data)
       await new Promise((resolve) => setTimeout(resolve, 2000))
 
       toast.success('Restaurante cadastrado com sucesso', {
